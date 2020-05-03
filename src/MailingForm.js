a75c948d4cd40554958a59a83160b0fbea62e10a
devky690 <60980607+devky690@users.noreply.github.com>
2020-05-03 19:58:48 -0400

    Add files via upload

--- src/MailingForm.js
+++ src/MailingForm.js
@@ -1,48 +1,51 @@
 import React from 'react'
 
-state = {
-    description: '',
-    mailinglist: '',
-}
-handleChange = e =>{
-    this.setState({
-        //update password and email by getting id
-        [e.target.id]: e.target.value
-    })
-}
-handleSubmit = e =>{
-    //prevents page refresh
-    e.preventDefault();
-    console.log(this.state);
-}
 
-const MailingForm =()=>{
-  return (
-    <div>
-<div className = "container" >
-    <form onSubmit = {this.handleSubmit} className = "white">
-        
-        <h5 className = "grey-text text-darken-3"> Stream Info</h5>
-        <div className = "input-field">
-            <label htmlFor = "Description">Description</label>
-            <input type="desc" id="desc" onChange = {this.handleChange}></input>
-         
-        </div>
-        <div className = "input-field">
-            <label htmlFor = "MailingList">MailingList</label>
-            <input type="email" id="email" onChange = {this.handleChange}></input>
-        </div>
-        <div className = "input-field">
-            <button className = "btn yellow darken-1 z-depth-0">Generate Key</button>
-        </div>
-        <div className = "input-field">
-            <button className = "btn yellow darken-1 z-depth-0">Start Broadcast</button>
-        </div>
-    </form>
-</div>
-</div>
-    
-  )
+class MailingForm extends React.Component{
+    state = {
+        description: '',
+        mailinglist: '',
+    }
+    handleChange = e =>{
+        this.setState({
+
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = e =>{
+        //prevents page refresh
+        e.preventDefault();
+        console.log(this.state);
+    }
+    render(){
+
+        return (
+      
+          
+          <div className = "container" >
+          <form onSubmit = {this.handleSubmit} className = "white">
+              
+              <h5 className = "grey-text text-darken-3"> Stream Info</h5>
+              <div className = "input-field">
+                  <label htmlFor = "Description">Description</label>
+                  <input type="desc" id="desc" onChange = {this.handleChange}></input>
+               
+              </div>
+              <div className = "input-field">
+                  <label htmlFor = "MailingList">MailingList</label>
+                  <input type="email" id="email" onChange = {this.handleChange}></input>
+              </div>
+              <div className = "input-field">
+                  <button className = "btn yellow darken-1 z-depth-0">Generate Key</button>
+              </div>
+              <div className = "input-field">
+                  <button className = "btn yellow darken-1 z-depth-0">Start Broadcast</button>
+              </div>
+          </form>
+      </div>
+      
+        )
+    }
 }
 
 export default MailingForm
